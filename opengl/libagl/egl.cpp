8a97a3138a7f27a0d705dcc97aef0cae22120d11
Xianzhong <b07117@freescale.com>
2013-07-31 00:38:06 +0800

    ENGR00273103 fixed i.MX6SL UI flicker with 2D acceleration
    3D draw and read surfaces should be bound in eglPostBufferVIV,
    if it's bound to a context, update the buffer for 3D EGL context
    
    Signed-off-by: Xianzhong <b07117@freescale.com>

--- opengl/libagl/egl.cpp
+++ opengl/libagl/egl.cpp
@@ -607,6 +607,9 @@ EGLClientBuffer egl_window_surface_v2_t::getRenderBuffer()
         if (!strncmp("surfaceflinger", buf, 14)) {
             isCompositor = true;
         }
+        else {
+            isCompositor = false;
+        }
     }
 
     if (isCompositor) {
@@ -2271,6 +2274,8 @@ EGLClientBuffer eglGetRenderBufferVIV(EGLClientBuffer handle)
 
 EGLBoolean eglPostBufferVIV(EGLClientBuffer buffer)
 {
+    EGLBoolean result = EGL_TRUE;
+
     EGLContext ctx = (EGLContext)getGlThreadSpecific();
     if (ctx == EGL_NO_CONTEXT) return EGL_FALSE;
     egl_context_t* c = egl_context_t::context(ctx);
@@ -2284,5 +2289,13 @@ EGLBoolean eglPostBufferVIV(EGLClientBuffer buffer)
         return EGL_FALSE;
     }
 
-    return d->swapBuffers();
+    result = d->swapBuffers();
+
+    // if it's bound to a context, update the buffer for 3D EGL context
+    if (d->ctx != EGL_NO_CONTEXT) {
+        d->bindDrawSurface((ogles_context_t*)d->ctx);
+        d->bindReadSurface((ogles_context_t*)d->ctx);
+    }
+
+    return result;
 }
