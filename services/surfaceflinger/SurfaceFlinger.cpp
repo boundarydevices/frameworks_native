a1cbf821645424355ba1fa09b1db15f10aed2618
Liu Xiaowen <b37945@freescale.com>
2013-11-06 16:07:25 +0800

    ENGR00215174 update to pass build for android 4.4.
    change drawingState() to getDrawingState().
    add getContentTransform() function to be used by LayerDim.
    
    Signed-off-by: Liu Xiaowen <b37945@freescale.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1218,6 +1218,7 @@ void SurfaceFlinger::postFramebuffer()
     uint32_t flipCount = getDefaultDisplayDevice()->getPageFlipCount();
     if (flipCount % LOG_FRAME_STATS_PERIOD == 0) {
         logFrameStats();
+    }
 
     if (mDebugFps) {
         debugShowFPS();
