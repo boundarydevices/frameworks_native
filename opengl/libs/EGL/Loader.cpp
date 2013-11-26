f4321f894993832c0011b00cbe502f313577723e
Richard Liu <r66033@freescale.com>
2013-11-26 14:09:35 +0800

    ENGR00289630 EGL load software opengl on mx6sl_evk
    load software opengl es on mx6sl_evk as no GPU 3D on the chip.
    On KitKat, egl loader will load SW opengl or HW opengl, and
    default it will skip load software opengl except on emulater,
    here we let egl loader load software opengl for mx6sl, and
    load hardware opengl for mx6dq.
    
    Conflicts:
    	opengl/libs/Android.mk
    
    Change-Id: I9f81928529039414537520b654414a1b77862556
    Signed-off-by: Richard Liu <r66033@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/373
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/905

--- opengl/libs/EGL/Loader.cpp
+++ opengl/libs/EGL/Loader.cpp
@@ -396,8 +396,11 @@ void *Loader::load_driver(const char* kind,
                         continue;
                     }
                     if (!strcmp(e->d_name, "libGLES_android.so")) {
+                        // don't skip software render if define USE_SW_OPENGL
+#ifndef USE_SW_OPENGL
                         // always skip the software renderer
                         continue;
+#endif
                     }
                     if (strstr(e->d_name, pattern.string()) == e->d_name) {
                         if (!strcmp(e->d_name + strlen(e->d_name) - 3, ".so")) {
