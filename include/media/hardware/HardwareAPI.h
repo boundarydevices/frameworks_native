3c79fbd252a5b02009462995ce04268dba8d27df
Jianzheng Zhou <jianzheng.zhou@freescale.com>
2013-12-18 17:49:27 +0800

    ENGR00290615 WifiDisplay:disable VPU reorder function
    Indroduce new index and struct to support disable VPU reorder
    feature from Nuplayer.
    
    Signed-off-by: Jianzheng Zhou <jianzheng.zhou@freescale.com>

--- include/media/hardware/HardwareAPI.h
+++ include/media/hardware/HardwareAPI.h
@@ -157,6 +157,16 @@ struct PrependSPSPPSToIDRFramesParams {
     OMX_BOOL bEnable;
 };
 
+// A pointer to this struct is passed to OMX_GetParameter when the extension
+// index for the 'OMX.google.android.index.disableAVCReorder'
+// extension is given.  The usage bits bDisable indicates that AVC reorder
+// should be enable or disable.
+struct DisableAVCReorderParams {
+    OMX_U32 nSize;
+    OMX_VERSIONTYPE nVersion;
+    OMX_BOOL bDisable;
+};
+
 }  // namespace android
 
 extern android::OMXPluginBase *createOMXPlugin();
