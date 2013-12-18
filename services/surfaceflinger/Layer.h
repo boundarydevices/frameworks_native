a4892a8d9fbcb80a696de9087b4ce9e04bad29d2
Richard Liu <r66033@freescale.com>
2013-12-18 18:01:36 +0800

    ENGR00292629 Dim layer process by hwcomposer on KitKat
    Dim layer not accelerated by hwcomposer on KitKat,
    root cause is setGeometry() and setPerFrameData()
    can't overload in LayerDim.cpp, need add virtual
    attribute in Layer.h, another change is use FloatRect
    instead of Rect for function LayerDim::computeCrop().
    
    Signed-off-by: Richard Liu <r66033@freescale.com>

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -183,9 +183,9 @@ protected:
 public:
     // -----------------------------------------------------------------------
 
-    void setGeometry(const sp<const DisplayDevice>& hw,
+    virtual void setGeometry(const sp<const DisplayDevice>& hw,
             HWComposer::HWCLayerInterface& layer);
-    void setPerFrameData(const sp<const DisplayDevice>& hw,
+    virtual void setPerFrameData(const sp<const DisplayDevice>& hw,
             HWComposer::HWCLayerInterface& layer);
     void setAcquireFence(const sp<const DisplayDevice>& hw,
             HWComposer::HWCLayerInterface& layer);
