18fc1198d4d40054647049f698aae551a185d4a3
Richard Liu <r66033@freescale.com>
2014-11-19 09:56:50 +0800

    MA-5877 fix screenshot not work issue on mx6sl_evk
    Root cause is some API change on Lollipop, NO_FBO_SCREENSHOT should
    do the same kind of change.
    
    Signed-off-by: Richard Liu <r66033@freescale.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3226,9 +3226,9 @@ status_t SurfaceFlinger::captureScreen(const sp<IBinder>& display,
 #include <GLES/glext.h>
 void SurfaceFlinger::renderScreenImplLocked(
         const sp<const DisplayDevice>& hw,
-        uint32_t reqWidth, uint32_t reqHeight,
+        Rect sourceCrop, uint32_t reqWidth, uint32_t reqHeight,
         uint32_t minLayerZ, uint32_t maxLayerZ,
-        bool yswap)
+        bool yswap, bool useIdentityTransform, Transform::orientation_flags rotation)
 {
     ATRACE_CALL();
 
@@ -3266,7 +3266,7 @@ void SurfaceFlinger::renderScreenImplLocked(
             if (state.z >= minLayerZ && state.z <= maxLayerZ) {
                 if (layer->isVisible()) {
                     if (filtering) layer->setFiltering(true);
-                    layer->draw(hw);
+                    layer->draw(hw, useIdentityTransform);
                     if (filtering) layer->setFiltering(false);
                 }
             }
@@ -3281,8 +3281,9 @@ void SurfaceFlinger::renderScreenImplLocked(
 status_t SurfaceFlinger::captureScreenImplLocked(
         const sp<const DisplayDevice>& hw,
         const sp<IGraphicBufferProducer>& producer,
-        uint32_t reqWidth, uint32_t reqHeight,
-        uint32_t minLayerZ, uint32_t maxLayerZ)
+        Rect sourceCrop, uint32_t reqWidth, uint32_t reqHeight,
+        uint32_t minLayerZ, uint32_t maxLayerZ,
+        bool useIdentityTransform, Transform::orientation_flags rotation)
 {
     ATRACE_CALL();
 
@@ -3314,7 +3315,7 @@ status_t SurfaceFlinger::captureScreenImplLocked(
 
     // and create the corresponding EGLSurface
     EGLSurface eglSurface = eglCreateWindowSurface(
-            mEGLDisplay, mEGLConfig, window, NULL);
+            mEGLDisplay, NULL, window, NULL);
     if (eglSurface == EGL_NO_SURFACE) {
         ALOGE("captureScreenImplLocked: eglCreateWindowSurface() failed 0x%4x",
                 eglGetError());
@@ -3328,8 +3329,8 @@ status_t SurfaceFlinger::captureScreenImplLocked(
         return BAD_VALUE;
     }
 
-    renderScreenImplLocked(hw, reqWidth, reqHeight, minLayerZ, maxLayerZ, false);
-
+    renderScreenImplLocked(hw, sourceCrop, reqWidth, reqHeight, minLayerZ, maxLayerZ, false,
+                            useIdentityTransform, rotation);
     // and finishing things up...
     if (eglSwapBuffers(mEGLDisplay, eglSurface) != EGL_TRUE) {
         ALOGE("captureScreenImplLocked: eglSwapBuffers() failed 0x%4x",
